85ac6aa00cc9fa983565d4cea4e82b74658f6e0c
mattm1346 <MATTM1346@gmail.com>
2021-06-27 21:46:59 +0000

    Add function for comparing hands and checkfor a draw

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,22 +9,28 @@ const game = ()=> {
 
         //Computer Random Choice
         const computerOptions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-
         options.forEach(option => {
             option.addEventListener("click", function(){
                 const computerNumber = Math.floor(Math.random() * 5);
                 const computerChoice = computerOptions[computerNumber];
-        
-            });
-            
-        });
+                
 
-        
+                //Update images on choice
+                playerHand.src = `./assets/images/${this.textContent}.png`;
+                computerHand.src = `./assets/image/${computerChoice}.png`;
+            });
+        });
+    };
     
+    const compareHands = (playerChoice, computerChoice) => {
+        //Update Outcome
+        const winner = document.querySelector('.outcome');
+        //Check for a draw
+        if(playerChoice === computerChoice){
+            winner.textContent = 'It is a draw';
+            return;
+        }
     }
-    
-    
-    
 
     playGame();
 }
