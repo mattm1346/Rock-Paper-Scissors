96b775f80d669761d8a6f7f06640d181d29d40a1
mattm1346 <MATTM1346@gmail.com>
2021-06-27 19:56:05 +0000

    Add main function 'game' to script.js and declare scores

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const game = ()=> {
+    let pScore = 0;
+    let cScore = 0;
+    //Play Game
+    const playGame = ()=> {
+        const options = document.querySelectorAll(".options-outer buttons");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+    }
+    playGame();
+    console.log(pScore);
+    console.log(cScore);
+}
+game();
