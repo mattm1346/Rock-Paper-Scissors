ecf73ca47cefcb7ad9c9190505e0a0c33c87f634
mattm1346 <MATTM1346@gmail.com>
2021-06-27 20:23:35 +0000

    Add computer choice functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,29 @@ const game = ()=> {
     let cScore = 0;
     //Play Game
     const playGame = ()=> {
-        const options = document.querySelectorAll(".options-outer buttons");
+        const options = document.querySelectorAll(".options");
         const playerHand = document.querySelector(".player-hand");
         const computerHand = document.querySelector(".computer-hand");
+
+        //Computer Random Choice
+        const computerOptions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+        options.forEach(option => {
+            option.addEventListener("click", function(){
+                const computerNumber = Math.floor(Math.random() * 5);
+                const computerChoice = computerOptions[computerNumber];
+        
+            });
+            
+        });
+
+        
+    
     }
+    
+    
+    
+
     playGame();
-    console.log(pScore);
-    console.log(cScore);
 }
 game();
