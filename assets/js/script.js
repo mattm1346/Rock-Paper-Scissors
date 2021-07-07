c9a5312bacb97f5d948ae61c4f2c8638c1af8ce4
mattm1346 <MATTM1346@gmail.com>
2021-07-07 13:36:10 +0000

    Add computer random choice function

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,14 +40,24 @@ const OPTION_CONFIG = {
         ]
     }
 };
-// Configure the level of difficulty
+//Configure the level of difficulty
 const LEVEL_CONFIG = {
     easy: [OPTION_CONFIG.rock, OPTION_CONFIG.paper, OPTION_CONFIG.scissors],
     medium: [OPTION_CONFIG.rock, OPTION_CONFIG.paper, OPTION_CONFIG.scissors, OPTION_CONFIG.lizard],
     hard: [OPTION_CONFIG.rock, OPTION_CONFIG.paper, OPTION_CONFIG.scissors, OPTION_CONFIG.lizard, OPTION_CONFIG.spock]
 };
-// Declare score of both players
+//Declare score of both players
 const game = ()=> {
     let playerScore = 0;
     let computerScore = 0;
+    //Assign hands
+    const playGame = (level)=> {
+        const options = LEVEL_CONFIG[level];
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+        //Computer Random Choice
+        const computerOptions = LEVEL_CONFIG[level];
+        const optionContainer = document.getElementsByClassName('options-outer')[0];
+        const optionNodes = document.querySelector(".options");
+    }
 };
